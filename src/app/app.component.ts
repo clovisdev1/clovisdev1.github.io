e8886bbf19875192a2d8c3884a6829f24578e327
clovisdev1 <112120433+clovisdev1@users.noreply.github.com>
2022-08-26 11:58:39 -0300

    acronym index, reorder name index, added counters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 import { Observable } from 'rxjs';
-import { Page } from './data/page-data'
+import { Page, Acr } from './data/page-data';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,22 +11,65 @@ import { Page } from './data/page-data'
 export class AppComponent {
   showspiner = false;
   title = 'copa';
-  item$: Observable<any[]>;
-  itemA$: Observable<any[]>;
+  listaAcr: Acr[] = [];
+  listaOrder: string[] = [];
+  listaAlfa: string[] = [];
+  lista: Observable<any[]>;
+  total : number = 0;
+  uniq : number = 0;
+  dups : number = 0;
+
   constructor(private firestore: AngularFirestore) {
-    this.item$ = firestore.collection('copa', ref => ref.orderBy('order')).valueChanges();
-    this.itemA$ = firestore.collection('copa', ref => ref.orderBy('name')).valueChanges();
-    //this.adddocs();
-  }
-  adddocs() {/*
-    let col = this.firestore.collection<Page>('copa');
-    let page : Page = {
-      order : 36,
-      name : 'Coca-cola',
-      stickers : ['C 1', 'C 2', 'C 3', 'C 4', 'C 5', 'C 6', 'C 7', 'C 8'],
-      qtds: [0, 0, 0, 0, 0, 0, 0, 0]
-    }
-    col.doc('Coca-cola').set(page);*/
+    this.lista = firestore.collection('copa', ref => ref.orderBy('order')).valueChanges();
+    firestore.collection("copa").get().forEach((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+          //console.log(`${doc.id} => ${doc.data()}`);
+          let data : Page = doc.data() as Page;
+          let stickerAcr = this.getAcr(data);
+          let orderAcr = stickerAcr;
+          if (stickerAcr == '000') {stickerAcr = '000'; orderAcr = '000'}
+          else if (data.stickers[0] == 'FWC 8') { stickerAcr = 'fwc8-18'; orderAcr = '001'}
+          else if (stickerAcr == 'FWC') { stickerAcr = 'fwc19-29'; orderAcr = 'zz1' }
+          else if (stickerAcr == 'C') { stickerAcr = 'Coca'; orderAcr = 'zz2';}
+          let pagetotal = data.stickers.length;
+          let pageuniq = 0;
+          let pagedups = 0;
+          for (let index = 0; index < data.stickers.length; index++) {
+            if (data.qtds[index] > 0) {
+              pageuniq++;
+              pagedups += (data.qtds[index]-1);
+            }
+          }
+          this.total += pagetotal;
+          this.uniq += pageuniq;
+          this.dups += pagedups;
+          if (this.listaAlfa.length == 0) this.listaAlfa.push(data.name);
+          else if (data.name == 'Museu' || (data.name == 'PaginaInicial') || (data.name == 'Estadios')) console.log();
+          else for (let i = 0; i < this.listaAlfa.length; i++) {
+            if (data.name < this.listaAlfa[i]) {
+              this.listaAlfa.splice(i, 0, data.name);
+              break;
+            } else if (i == this.listaAlfa.length - 1) {
+              this.listaAlfa.push(data.name);
+              break;
+            }
+          }
+          if (this.listaAcr.length == 0) {
+            this.listaAcr.push({ acr : stickerAcr, name : data.name, total : pagetotal, uniq : pageuniq, dups : pagedups });
+            this.listaOrder.push(orderAcr);
+          } else for (let i = 0; i < this.listaAcr.length; i++) {
+            if (orderAcr < this.listaOrder[i]) {
+              this.listaAcr.splice(i, 0, { acr : stickerAcr, name : data.name, total : pagetotal, uniq : pageuniq, dups : pagedups });
+              this.listaOrder.splice(i, 0, orderAcr);
+              break;
+            } else if (i == this.listaAcr.length - 1) {
+              this.listaAcr.push({ acr : stickerAcr, name : data.name, total : pagetotal, uniq : pageuniq, dups : pagedups });
+              this.listaOrder.push(orderAcr);
+              break;
+            }
+          }
+      });
+    });
   }
   getQtd(sticker : string, item : Page) {
     let i = item.stickers.indexOf(sticker);
@@ -53,5 +96,23 @@ export class AppComponent {
   showvars(item : any) {
     console.log(JSON.stringify(item));
     return "";
+  }
+  getAcr(item : Page) {
+    let a = item.stickers[0];
+    let a1 = a.split(' ');
+    return a1[0];
+  }
+  topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
+  getPageDesc(page: Page) {
+    for (let i = 0; i < this.listaAcr.length; i++) {
+      let acrItem = this.listaAcr[i];
+      if (acrItem.name == page.name) {
+        return page.name + ' tt: ' + acrItem.total + ' - t: ' + acrItem.uniq + ' - r: ' + acrItem.dups;
+      }
+    };
+    return page.name;
   }
 }
--- src/app/data/page-data.ts
+++ src/app/data/page-data.ts
@@ -1,6 +1,15 @@
+import { NonNullableFormBuilder } from "@angular/forms";
+
 export interface Page {
   name: string;
   order: number;
   stickers: string[];
   qtds: number[];
+}
+export interface Acr {
+  name : string;
+  acr : string;
+  total : number;
+  uniq : number;
+  dups : number;
 }
